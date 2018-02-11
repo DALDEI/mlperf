5f3b188b68ad340bb08ce590b1fcfa3b7447f468
David Lee <dlee@nexstra.com>
2018-02-11 07:45:01 -0500

    Simplify the connection reset logic

--- src/main/kotlin/com/calldei/mlperf/javaapi/Main.kt
+++ src/main/kotlin/com/calldei/mlperf/javaapi/Main.kt
@@ -9,162 +9,170 @@ import com.marklogic.xcc.ContentSourceFactory
 import com.marklogic.xcc.SecurityOptions
 import com.marklogic.xcc.Session
 import java.net.URI
+import java.util.logging.Level
+import java.util.logging.LogManager
+import java.util.logging.Logger
 import javax.xml.crypto.Data
 
-fun <T> T.reset() : Unit {
-    if( this is DatabaseClient )
-        this.release()
-    else
-        if( this is Session ){
-            this.close()
-        }
-    else
-            Unit
+val log = Logger.getLogger("com.calldei.mlperf.javaapi")
+fun <T> T.reset(): Unit {
+  if (this is DatabaseClient)
+    this.release()
+  else
+    if (this is Session) {
+      this.close()
+    } else
+      Unit
 }
 
-class TestClient<T>( val config: Config , val init : () -> T )  {
-    var resetCount = 0
-    var client : T = init()
+var results = System.out
 
-    fun resetClient() : Unit {
-        if (config.resetConnection ) {
-            client.reset()
-            System.err.println("Reseting Client Connection")
-            client = init()
-        }
+class TestClient<T>(val config: Config, val init: () -> T) {
+  var resetCount = 0
+  var client: T = init()
+
+  fun resetClient(): Unit {
+    if (config.resetConnection) {
+      client.reset()
+      log.log(Level.FINE, "Reseting Client Connection")
+      client = init()
     }
+  }
 
-    fun runPerf(title: String, chunksz: Int, test: () -> Int) {
-        resetClient()
-        System.err.println("Running ${title}")
-        val start = System.nanoTime()
-        var n: Int = 0
+  fun runPerf(title: String, chunksz: Int, test: () -> Int) {
+    resetClient()
+    log.log(Level.INFO, "Running ${title}...")
+    val start = System.nanoTime()
+    var n: Int = 0
 
-        run {
-            n = test()
-         }
-        val end = System.nanoTime()
-        val elapse = (end - start).toDouble() / 1000_000.0
-        println("${title}\t${n}\t${chunksz}\t${elapse}\t${n.toDouble() / (elapse / 1000.0)}")
-
+    run {
+      n = test()
     }
+    val end = System.nanoTime()
+    val elapse = (end - start).toDouble() / 1000_000.0
+    results.println("${title}\t${n}\t${chunksz}\t${elapse}\t${n.toDouble() / (elapse / 1000.0)}")
+
+  }
 }
 
 
 fun main(args: Array<String>) {
-    val config = Config()
+  val config = Config()
 
-    val jc = JCommander.newBuilder().addObject(config).build()
-    // add if using java9
-    //  jc.usageFormatter = com.beust.jcommander.UnixStyleUsageFormatter(jc)
-    try {
-        jc.parse(*args)
-        if (config.help) throw ParameterException("")
-        runTests(config)
-    } catch (e: ParameterException) {
-        System.err.println(e.message)
-        jc.usage()
+  val jc = JCommander.newBuilder().addObject(config).build()
+  // add if using java9
+  //  jc.usageFormatter = com.beust.jcommander.UnixStyleUsageFormatter(jc)
+  try {
+    jc.parse(*args)
+    if (config.help) throw ParameterException("")
+    runTests(config)
+  } catch (e: ParameterException) {
+    println(e.message)
+    jc.usage()
 
-    } catch (e: Exception) {
-        System.err.println(e)
-    }
+  } catch (e: Exception) {
+    log.log(Level.SEVERE, "Exception running test", e)
+  }
 
 }
-fun newSession(config: Config ): Session {
-    val xcc = when( config.ssl ) {
-        true -> "xccs"
-        else -> "xcc"
-    }
-    // XCC API
-    val uri = URI("${xcc}://${config.user}:${config.password}@${config.host}:${config.port}")
-    val cs = ContentSourceFactory.newContentSource(uri,
-            if( config.ssl )
-                SecurityOptions(getSSLContext())
-            else null )
-    val session = cs.newSession()
-    return session
+
+fun newSession(config: Config): Session {
+  val xcc = when (config.ssl) {
+    true -> "xccs"
+    else -> "xcc"
+  }
+  // XCC API
+  val uri = URI("${xcc}://${config.user}:${config.password}@${config.host}:${config.port}")
+  val cs = ContentSourceFactory.newContentSource(uri,
+      if (config.ssl)
+        SecurityOptions(getSSLContext())
+      else null)
+  val session = cs.newSession()
+  return session
 }
 
 
 fun runTests(config: Config) {
-    val npojos = config.documents
-    val ninner = config.innersz
-    val chunksz = config.chuncksz
-    System.err.println("Running test set with ${config.documents} documents, inner array size ${config.innersz} chunk size ${chunksz}")
-    // Array of Java Objects
-    val pojos = Array<POJO>(npojos) { newPOJO(randLong(), ninner) }
-    val om = jacksonObjectMapper()
-    // mapOf  "id" to "{ serialized JSON }"
-    val pojoMap = pojos.map { it.id to om.writeValueAsString(it) }.toMap()
-    val pojoNodes = pojos.map { om.valueToTree<JsonNode>(it) }.toTypedArray()
+  val npojos = config.documents
+  val ninner = config.innersz
+  val chunksz = config.chuncksz
+  val ssl = if (config.ssl) "Using SSL" else "NOT Using SSL"
+  val doReset = if (config.resetConnection) "Resetting connection after every test" else ""
 
+  log.info("Running test to host ${config.host}:${config.port} set with ${config.documents} documents, inner array size ${config.innersz} chunk size ${chunksz} ${ssl} ${doReset} ")
+  // Array of Java Objects
+  val pojos = Array<POJO>(npojos) { newPOJO(randLong(), ninner) }
+  val om = jacksonObjectMapper()
+  // mapOf  "id" to "{ serialized JSON }"
+  val pojoMap = pojos.map { it.id to om.writeValueAsString(it) }.toMap()
+  // arrayOf JsonNode's of POJO
+  val pojoNodes = pojos.map { om.valueToTree<JsonNode>(it) }.toTypedArray()
 
-    println("test\tdocs\tchunks\telapsed ms\tdocs/sec")
-    if (!config.noRunPojo) { // POJO and Document API
-        TestClient<DatabaseClient>(config, { newClient(config) }).run {
-            try {
-                runPerf("writePOJONoop", 1) {
-                    writePojoNoopEval( pojos)
-                }
-
-                runPerf("writePOJO", 1) {
-                    writePOJOs( pojos)
-                }
-                runPerf("writePOJOAsDataBind", 1) {
-                    writePojoAsDatabind( pojos)
-                }
-
-                runPerf("writePojoAsDatabindChunked", chunksz) {
-                    writePojoAsDatabindChunked( pojos, chunksz)
-                }
-                runPerf("writePojoAsDatabindEval", 1) {
-                    writePojoAsDatabindEval( pojos)
-                }
-                runPerf("writePojoAsEvalString", 1) {
-                    writePojoAsEvalString( pojoMap)
-                }
-                runPerf("writePojoAsDatabindEvalChunked", chunksz) {
-                    writePojoAsDatabindEvalChunked( pojos, chunksz)
-                }
-            } finally {
-                client.reset()
-            }
+  println("test\tdocs\tchunks\telapsed ms\tdocs/sec")
+  if (!config.noRunPojo) { // POJO and Document API
+    TestClient<DatabaseClient>(config, { newClient(config) }).run {
+      try {
+        runPerf("writePOJONoop", 1) {
+          writePojoNoopEval(pojos)
         }
+
+        runPerf("writePOJO", 1) {
+          writePOJOs(pojos)
+        }
+        runPerf("writePOJOAsDataBind", 1) {
+          writePojoAsDatabind(pojos)
+        }
+
+        runPerf("writePojoAsDatabindChunked", chunksz) {
+          writePojoAsDatabindChunked(pojos, chunksz)
+        }
+        runPerf("writePojoAsDatabindEval", 1) {
+          writePojoAsDatabindEval(pojos)
+        }
+        runPerf("writePojoAsEvalString", 1) {
+          writePojoAsEvalString(pojoMap)
+        }
+        runPerf("writePojoAsDatabindEvalChunked", chunksz) {
+          writePojoAsDatabindEvalChunked(pojos, chunksz)
+        }
+      } finally {
+        client.reset()
+      }
     }
-        if (!config.noRunXdbc) {
+  }
+  if (!config.noRunXdbc) {
 
-            TestClient<Session>(config, { newSession(config) }).run {
-                try {
-                    runPerf("xccWriteJSONAsNoop", 1) {
-                        xccWriteJSONAsNoop( pojoNodes)
+    TestClient<Session>(config, { newSession(config) }).run {
+      try {
+        runPerf("xccWriteJSONAsNoop", 1) {
+          xccWriteJSONAsNoop(pojoNodes)
 
-                    }
-                    runPerf("xccWriteJSON", chunksz) {
-                        xccWriteJSON( pojoNodes)
+        }
+        runPerf("xccWriteJSON", chunksz) {
+          xccWriteJSON(pojoNodes)
+        }
+        runPerf("xccWriteJSONAsString", 1) {
+          xccWriteJSONAsString(pojoMap)
+        }
+        runPerf("xccWriteJSONChunked", chunksz) {
+          xccWriteJSONChunked(pojoNodes, chunksz)
+        }
+        runPerf("xccWriteJSONChunked2", chunksz) {
+          xccWriteJSONChunked2(pojoNodes, chunksz)
+        }
+        runPerf("xccWriteJSONAsStringChunked", chunksz) {
+          xccWriteJSONAsStringChunked(pojoMap, chunksz)
+        }
 
-                    }
-                    runPerf("xccWriteJSONAsString", 1) {
-                        xccWriteJSONAsString( pojoMap)
-                    }
-                    runPerf("xccWriteJSONChunked", chunksz) {
-                        xccWriteJSONChunked( pojoNodes, chunksz)
-                    }
-                    runPerf("xccWriteJSONChunked2", chunksz) {
-                        xccWriteJSONChunked2( pojoNodes, chunksz)
-                    }
-                    runPerf("xccWriteJSONAsStringChunked", chunksz) {
-                        xccWriteJSONAsStringChunked( pojoMap, chunksz)
-                    }
-
-                    runPerf("xccWriteJSONAsEval", 1) {
-                        xccWriteJSONAsEval( pojoNodes)
-                    }
-                    runPerf("xccWriteJSONAsEvalChunked", chunksz) {
-                        xccWriteJSONAsEvalChunked( pojoNodes, chunksz)
-                    }
-                } finally {
-                    client.reset()
-                }
-            }
+        runPerf("xccWriteJSONAsEval", 1) {
+          xccWriteJSONAsEval(pojoNodes)
+        }
+        runPerf("xccWriteJSONAsEvalChunked", chunksz) {
+          xccWriteJSONAsEvalChunked(pojoNodes, chunksz)
+        }
+      } finally {
+        client.reset()
+      }
     }
+  }
 }
--- src/main/kotlin/com/calldei/mlperf/javaapi/Pojo.kt
+++ src/main/kotlin/com/calldei/mlperf/javaapi/Pojo.kt
@@ -3,29 +3,39 @@ package com.calldei.mlperf.javaapi
 import com.marklogic.client.DatabaseClient
 import com.marklogic.client.io.JacksonDatabindHandle
 
-private fun getURI( tag: String , id: Long ) = "/java/${tag}/doc-${id}.json"
-private fun getURI( tag: String , pojo: POJO) = getURI(tag, pojo.id)
+private fun getURI(tag: String, id: Long) = "/java/${tag}/doc-${id}.json"
+private fun getURI(tag: String, pojo: POJO) = getURI(tag, pojo.id)
 
 
 /*
  * JAVA Client API
  */
 
+fun evalNop(client: DatabaseClient, count: Int): Int {
+  var n = 0
+  (1..count).forEach {
+
+    val repo = client.newServerEval()
+    repo.xquery("1")
+    repo.eval().forEach { n++ }
+  }
+  return n
+}
 
 
 /*
  Common simple use: Write each POJO as a separate request
  */
 
-fun TestClient<DatabaseClient>.writePOJOs( pojos : Array<POJO> ) : Int{
+fun TestClient<DatabaseClient>.writePOJOs(pojos: Array<POJO>): Int {
 
-    val repo = client.newPojoRepository(POJO::class.java, java.lang.String::class.java)
-    var n=0
-    pojos.forEach {
-        repo.write(it)
-        n++
-    }
-    return n
+  val repo = client.newPojoRepository(POJO::class.java, java.lang.String::class.java)
+  var n = 0
+  pojos.forEach {
+    repo.write(it)
+    n++
+  }
+  return n
 
 }
 
@@ -33,34 +43,34 @@ fun TestClient<DatabaseClient>.writePOJOs( pojos : Array<POJO> ) : Int{
  * Use explict Jackson Data binding
  */
 
-fun TestClient<DatabaseClient>. writePojoAsDatabind(pojos : Array<POJO>  ) : Int{
+fun TestClient<DatabaseClient>.writePojoAsDatabind(pojos: Array<POJO>): Int {
 
-    val repo = client.newJSONDocumentManager()
-    var n=0
-    pojos.asSequence().forEach {
-
-        repo.writeAs(getURI("databind", it),JacksonDatabindHandle<POJO>(it))
-        n++
-    }
-    return n
+  val repo = client.newJSONDocumentManager()
+  var n = 0
+  pojos.asSequence().forEach {
+    repo.writeAs(getURI("databind", it), JacksonDatabindHandle<POJO>(it))
+    n++
+  }
+  return n
 }
+
 /*
  * Use explict Jackson Data binding in batches of chunksz
  */
-fun TestClient<DatabaseClient>.writePojoAsDatabindChunked(pojos : Array<POJO>, chunksz: Int) : Int{
+fun TestClient<DatabaseClient>.writePojoAsDatabindChunked(pojos: Array<POJO>, chunksz: Int): Int {
 
-    val repo = client.newJSONDocumentManager()
-    var n=0
-      pojos.asSequence().chunked(chunksz ).forEach { list ->
-          val set = repo.newWriteSet()
-          list.forEach {
-              set.add(getURI("dbchunk", it), JacksonDatabindHandle<POJO>(it))
-              n++
-          }
-          repo.write(set)
+  val repo = client.newJSONDocumentManager()
+  var n = 0
+  pojos.asSequence().chunked(chunksz).forEach { list ->
+    val set = repo.newWriteSet()
+    list.forEach {
+      set.add(getURI("dbchunk", it), JacksonDatabindHandle<POJO>(it))
+      n++
+    }
+    repo.write(set)
 
-      }
-    return n
+  }
+  return n
 }
 
 /*
@@ -70,40 +80,38 @@ fun TestClient<DatabaseClient>.writePojoAsDatabindChunked(pojos : Array<POJO>, c
  * this produces a different form then the Java API
  */
 
-fun TestClient<DatabaseClient>.writePojoAsEvalString( pojos : Map<Long,String> ) : Int{
+fun TestClient<DatabaseClient>.writePojoAsEvalString(pojos: Map<Long, String>): Int {
 
-    var n=0
+  var n = 0
 
-    pojos.entries.forEach() {(id,str) ->
-        val repo = client.newServerEval()
-        repo.addVariable("url", getURI("eval", id))
-        repo.addVariable("content",(str))
-        repo.xquery("""
-    declare variable  ${'$'}url as xs:string external ;
-    declare variable ${'$'}content external ;
-   xdmp:document-insert( ${'$'}url , xdmp:unquote( ${'$'}content ) )
-            """)
+  pojos.entries.forEach() { (id, str) ->
+    val repo = client.newServerEval()
+    repo.addVariable("url", getURI("eval", id))
+    repo.addVariable("content", (str))
+    repo.xquery(
+        "declare variable  \$url as xs:string external ;"+
+              "declare variable ${'$'}content external ;"+
+              "xdmp:document-insert( \$url , xdmp:unquote( \$content ) )"
+    )
 
-        repo.eval().forEach {
-            println(it.string)
-
-
-        }
-        n++
+    repo.eval().forEach {
+      println(it.string)
     }
-    return n
+    n++
+  }
+  return n
 }
 
 /*
  * Minimal round trip using eval()
  */
-fun TestClient<DatabaseClient>.writePojoNoopEval(pojos : Array<POJO> ) : Int {
-    var n = 0
-    pojos.forEach {
-        val repo = client.newServerEval()
-        val res = repo.xquery("1").eval().forEach { n += it.number.toInt()  }
-    }
-    return n
+fun TestClient<DatabaseClient>.writePojoNoopEval(pojos: Array<POJO>): Int {
+  var n = 0
+  pojos.forEach {
+    val repo = client.newServerEval()
+    val res = repo.xquery("1").eval().forEach { n += it.number.toInt() }
+  }
+  return n
 
 }
 
@@ -111,56 +119,45 @@ fun TestClient<DatabaseClient>.writePojoNoopEval(pojos : Array<POJO> ) : Int {
  * Use the Java API Jackson Databind Handle which encodes the JSON as a json:object
  * Convert to JSON Node in the server and insert
  */
-fun TestClient<DatabaseClient>.writePojoAsDatabindEval(pojos : Array<POJO> ) : Int{
+fun TestClient<DatabaseClient>.writePojoAsDatabindEval(pojos: Array<POJO>): Int {
+  var n = 0
+  pojos.forEach {
+    val repo = client.newServerEval()
+    repo.addVariable("url", getURI("evalbind", it))
+    repo.addVariable("content", (JacksonDatabindHandle(it)))
+    repo.xquery(
+    "declare variable ${'$'}url as xs:string external ;" +
+    "declare variable \$content external ;"+
+    "xdmp:document-insert( \$url , xdmp:to-json( \$content ) )"
+    )
 
-    var n=0
-    pojos.forEach {
-        val repo = client.newServerEval()
-        repo.addVariable("url", getURI("evalbind", it))
-        repo.addVariable("content",(JacksonDatabindHandle(it)))
-        repo.xquery("""
-    declare variable ${'$'}url as xs:string external ;
-    declare variable ${'$'}content external ;
-    xdmp:document-insert( ${'$'}url , xdmp:to-json( ${'$'}content ) )
-            """)
-
-        repo.eval().forEach {
-            println(it.string)
-        }
-        n++
+    repo.eval().forEach {
+      println(it.string)
     }
-    return n
+    n++
+  }
+  return n
 }
+
 /*
  * Write batches of JSON Objects by sending an array of json-object's
  * serialized using the JacksonDatabindHandle
  */
-fun TestClient<DatabaseClient>.writePojoAsDatabindEvalChunked(pojos : Array<POJO>, chunksz: Int  ) : Int{
+fun TestClient<DatabaseClient>.writePojoAsDatabindEvalChunked(pojos: Array<POJO>, chunksz: Int): Int {
 
-    var n=0
-    pojos.asSequence().chunked(chunksz ).forEach { list ->
-        val repo = client.newServerEval()
-        repo.addVariable("rooturl","/javaeval/")
-        repo.addVariable("content",(JacksonDatabindHandle(list)))
-        /* // Insert N docuemnts using 'id' to form the URL
-
-declare variable $rooturl as xs:string external ;
-declare variable \$content external ;
-for $c in json:array-values($content) +
-   return xdmp:document-insert(
-      $rooturl || map:get($c,'id') || '.json', xdmp:to-json( $c ) )
-
-
-        // Really ugly but better then """ ${'$'}var """
-   */
-        repo.xquery("" +
-    "declare variable \$rooturl as xs:string external ;" +
-    "declare variable \$content external ; " +
-    "for \$c in json:array-values(\$content)" +
-     " return xdmp:document-insert( \$rooturl || map:get(\$c,'id') || '.json', xdmp:to-json( \$c ) )"
-        )
-        repo.eval()
-        n += list.size
-    }
-    return n
+  var n = 0
+  pojos.asSequence().chunked(chunksz).forEach { list ->
+    val repo = client.newServerEval()
+    repo.addVariable("rooturl", "/javaeval/")
+    repo.addVariable("content", (JacksonDatabindHandle(list)))
+    repo.xquery("" +
+        "declare variable \$rooturl as xs:string external ;" +
+        "declare variable \$content external ; " +
+        "for \$c in json:array-values(\$content)" +
+        " return xdmp:document-insert( \$rooturl || map:get(\$c,'id') || '.json', xdmp:to-json( \$c ) )"
+    )
+    repo.eval()
+    n += list.size
+  }
+  return n
 }
